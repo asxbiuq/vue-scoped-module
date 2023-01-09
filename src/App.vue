b2f4aac7ef8b6f6d2524e77b3cb8a02587dcd0c8
hvft <MICHAL77147397@gmail.com>
2023-01-09 11:09:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="$style.wrapper">
+    <p>parent</p>
+    <Child />
+  </div>
+</template>
+
+<script setup>
+  import Child from './components/Child.vue'
+</script>
+<style module>
+  .wrapper {
+    outline: 2px solid blue;
+  }
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :class="$style.wrapper">
+    <p>child</p>
+  </div>
+</template>
+
+<style module>
+  .wrapper {
+    border: 2px solid orange;
+  }
+</style>
